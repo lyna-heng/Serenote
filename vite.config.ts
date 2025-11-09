621b184679c5254a169eedef770d94e7516a5c52
lyna-heng <elynahenggrundy@gmail.com>
2025-11-09 03:21:52 +0100

    testing github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/Serenote",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
